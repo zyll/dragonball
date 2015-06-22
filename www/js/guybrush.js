8b4940de7261e636b1064bacfd16b9bb713edb06
zyll <gilles.robit@af83.com>
2015-06-22 22:18:32 +0200

    [fix] 2 pawns at the same time

--- www/js/guybrush.js
+++ www/js/guybrush.js
@@ -4,27 +4,11 @@ function normalize(touch) {
   return {x: touch.pageX, y: touch.pageY, id: touch.identifier+''};
 }
 
-function event2coord(event) {
-  var tupplet = [];
-  var touchs = event.originalEvent.changedTouches;
-  for(var i = 0; i < touchs.length; i++) {
-    tupplet.push(normalize(touchs[i]));
-  }
-  return tupplet;
-}
-
-function distanceTupplet(tupplet) {
-  return [
-    distance(tupplet[0], tupplet[1]),
-    distance(tupplet[0], tupplet[2]),
-    distance(tupplet[1], tupplet[2]),
-  ].sort(ascFloatSort);
-}
 function ascFloatSort(a, b) {
-    if(a < b) return -1;
-    if(a > b) return 1;
-    return 0;
-  }
+  if(a < b) return -1;
+  if(a > b) return 1;
+  return 0;
+}
 
 function distance(a, b) {
   return Math.sqrt(Math.pow((b.x - a.x), 2) + Math.pow(b.y - a.y, 2));
@@ -48,12 +32,13 @@ function end(event) {
   event.preventDefault();
   return [].map.call(event.originalEvent.changedTouches, function(touch) {;
     delete contacts[touch.identifier];
-    return {id: touch.identifieri+''};
+    return {id: touch.identifier+''};
   });
 }
 
-function nearestFrom(pawn, keys) {
-  if(keys.length < 3 || keys.indexOf(pawn.id) < 0) return [];
+function nearestFrom(keys) {
+  var pawn = contacts[keys[0]];
+  if(keys.length < 3) return [];
   var m = keys.map(function(to) {
     return {
       id: contacts[to].id,
@@ -68,32 +53,20 @@ function nearestFrom(pawn, keys) {
 }
 
 function checkForPawn(changed) {
+  var i, next;
   var pawns = [];
   var keys = Object.keys(contacts);
-  var pawn = nearestFrom(changed.shift(), keys);
-  console.log(pawn);
-  while(pawn.length === 3) {
+  var pawn = nearestFrom(keys);
+  while(pawn.length) {
     pawns.push(pawn);
     pawn.forEach(function(p) { // remove used contact
-      var i;
-      if(changed.length) {
-        i = changed.indexOf(p.id);
-        if(i >= 0 ) changed.splice(i, 1);
-      }
-      var i;
-      if(keys.length) {
-        i = keys.indexOf(p.id);
-        if(i >= 0 ) keys.splice(i, 1);
-      }
+      keys.splice(keys.indexOf(p.id), 1);
     });
-    pawn = nearestFrom(changed.shift() || keys[0], keys);
+    pawn = nearestFrom(keys);
   }
   return pawns;
 }
 
-function has3side(d) {
-  return d.length === 3;
-}
 var contacts = {};
 var pawns = [];
 
